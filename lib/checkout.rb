befc28691a5f9962da8539a5ab04cb61a8a98002
allezz <aerbay@gmail.com>
2020-04-10 11:33:56 +0200

    Create subtot method

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -28,34 +28,33 @@ class Checkout
 
 
     def total
-        subbill=0
-        subbill1=0
+        @subbill=0
+        @subbill1=0
         if @promotions[:promo1] == false && @promotions[:promo2] == false
-         
-            @scanned.each do |i|
-                @bill+=i[:price]
-            end
-
+            subtot()
         elsif @promotions[:promo1] == true && @promotions[:promo2] == false
-            @scanned.each do |i|
-                subbill+=i[:price]
-            end
-            subbill > 60 ? @bill=(subbill*0.9).round(2) : @bill=subbill.round(2)
+            subtot()
+            @subbill > 60 ? @bill=(@subbill*0.9).round(2) : @bill=@subbill.round(2)
         elsif @promotions[:promo1] == false && @promotions[:promo2] == true  
-            @scanned.each do |i|
-                subbill+=i[:price]      
-            end
+            subtot()
             number_ties = @scanned.count(@items_in_store[0])
-            number_ties > 1 ? @bill=(subbill-0.75*number_ties).round(2) : @bill=subbill.round(2)
-    
+            number_ties > 1 ? @bill=(@subbill-0.75*number_ties).round(2) : @bill=@subbill.round(2)
         else 
-            @scanned.each do |i|
-                subbill1+=i[:price]      
-            end
+            subtot()
             number_ties = @scanned.count(@items_in_store[0])
-            number_ties > 1 ? subbill2=subbill1-0.75*number_ties : subbill2=subbill1
+            number_ties > 1 ? subbill2=@subbill1-0.75*number_ties : subbill2=@subbill1
             subbill2 > 60 ? @bill=(subbill2*0.9).round(2) : @bill=subbill2.round(2)
         end
+    end
+
+    private
+
+    def subtot()
+        @scanned.each do |i|
+            @bill+=i[:price]
+        end
+        @subbill=@bill
+        @subbill1=@bill
     end
 
 
